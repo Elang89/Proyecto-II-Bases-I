c524d59b506842aff37b04ab024de47e8ec1f392
ricardo-cr23 <ricardo.cr023@gmail.com>
2015-06-03 01:29:43 -0600

    Update Species_Search-Procedures.sql

--- MySQL/Scripts/Species_Search-Procedures.sql
+++ MySQL/Scripts/Species_Search-Procedures.sql
@@ -6,7 +6,7 @@ CREATE DEFINER=`DBadmin`@`localhost` PROCEDURE `find_my_species`(
         p_User_ID INT(11)
 )
 BEGIN
-        SELECT Species_Id, Specie_Name, Size_Name, Habitat_Name, Beak_Name, Color_Name, Quantity, Gender_Name, Family_Name, Sub_Order_Name, Order_Name, Class_Name, Description
+        SELECT Species_Id, Specie_Name, ScientificName, Size_Name, Habitat_Name, Beak_Name, Color_Name, Quantity, Gender_Name, Family_Name, Sub_Order_Name, Order_Name, Class_Name, Description
         FROM species_found, specie, Size, Habitat, Beak_Type, Color, offspring_quantity, Gender, Family, Sub_Order, Orders, Class
         WHERE FK_User_Id = p_User_ID
         AND specie.Specie_Id = species_found.FK_Specie_Id 
@@ -24,7 +24,7 @@ END
 CREATE DEFINER=`DBadmin`@`localhost` PROCEDURE `find_all_species`(
 )
 BEGIN
-        SELECT Species_Id, Specie_Name, Size_Name, Habitat_Name, Beak_Name, Color_Name, Quantity, Gender_Name, Family_Name, Sub_Order_Name, Order_Name, Class_Name, Description
+        SELECT Species_Id, Specie_Name, ScientificName, Size_Name, Habitat_Name, Beak_Name, Color_Name, Quantity, Gender_Name, Family_Name, Sub_Order_Name, Order_Name, Class_Name, Description
         FROM species_found, specie, Size, Habitat, Beak_Type, Color, offspring_quantity, Gender, Family, Sub_Order, Orders, Class
         WHERE specie.Specie_Id = species_found.FK_Specie_Id 
         AND specie.FK_Size_Id = Size.Size_Id 
@@ -44,20 +44,22 @@ CREATE DEFINER=`DBadmin`@`localhost` PROCEDURE `find_species_detail`(
         p_Species_ID INT(11)
 )
 BEGIN
-        SELECT Species_Id, Specie_Name, Size_Name, Habitat_Name, Beak_Name, Color_Name, Quantity, Gender_Name, Family_Name, Sub_Order_Name, Order_Name, Class_Name, Description, Register_Date, Username, First_Name, Last_Name
+        SELECT Species_Id, Specie_Name, ScientificName, Size_Name, Habitat_Name, Beak_Name, Color_Name, Quantity, Gender_Name, Family_Name, Sub_Order_Name, Order_Name, Class_Name, Description, Register_Date, Username, First_Name, Last_Name
         FROM species_found, specie, Size, Habitat, Beak_Type, Color, offspring_quantity, Gender, Family, Sub_Order, Orders, Class, user_table
         WHERE Specie_ID = p_Species_ID
-        AND specie.Specie_Id = species_found.FK_Specie_Id 
-        AND specie.FK_Size_Id = Size.Size_Id 
-        AND specie.FK_Color_Id = Color.Color_Id 
-        AND specie.FK_Habitat_Id = habitat.Habitat_Id
-        AND specie.FK_Offspring_Quantity_Id = offspring_quantity.Offspring_Quantity_Id  
-        AND specie.FK_Gender_Id = Gender.Gender_Id  
-        AND Gender.FK_Family_Id = Family.Family_Id 
-        AND Family.FK_Sub_Order_Id = sub_order.Sub_Order_Id 
-        AND sub_order.FK_Order_Id = Orders.Order_Id 
-        AND Orders.FK_Class_Id = Class.Class_Id;
-END 
+        AND Specie.Specie_Id = Species_Found.FK_Specie_ID 
+		AND User_Table.User_Id = Species_Found.FK_User_ID
+		AND Specie.FK_Size_ID = Size.Size_ID
+		AND specie.FK_Habitat_Id = habitat.Habitat_Id
+		AND Specie.FK_Beak_Type_ID = Beak_Type.Beak_Type_ID
+		AND specie.FK_Color_Id = Color.Color_Id
+		AND specie.FK_Offspring_Quantity_Id = offspring_quantity.Offspring_Quantity_Id
+		AND specie.FK_Gender_Id = Gender.Gender_Id
+		AND Gender.FK_Family_Id = Family.Family_Id 
+		AND Family.FK_Sub_Order_Id = sub_order.Sub_Order_Id 
+		AND Sub_Order.FK_Order_Id = Orders.Order_Id 
+		AND Orders.FK_Class_Id = Class.Class_Id;
+END
 
 CREATE DEFINER=`DBadmin`@`localhost` PROCEDURE `find_bird`(
 	p_Specie_Name VARCHAR(45),
