18c26c39104a230805fc790fecd361af17c7c0c3
ricardo-cr23 <ricardo.cr023@gmail.com>
2015-06-03 01:16:06 -0600

    Find_Bird corrected
    Now it does the search correctly and without errors in data.

--- MySQL/Scripts/Species_Search-Procedures.sql
+++ MySQL/Scripts/Species_Search-Procedures.sql
@@ -19,7 +19,7 @@ BEGIN
         AND Family.FK_Sub_Order_Id = sub_order.Sub_Order_Id 
         AND sub_order.FK_Order_Id = Orders.Order_Id 
         AND Orders.FK_Class_Id = Class.Class_Id;
-END 
+END
 
 CREATE DEFINER=`DBadmin`@`localhost` PROCEDURE `find_all_species`(
 )
@@ -31,11 +31,13 @@ BEGIN
         AND specie.FK_Color_Id = Color.Color_Id 
         AND specie.FK_Habitat_Id = habitat.Habitat_Id
         AND specie.FK_Offspring_Quantity_Id = offspring_quantity.Offspring_Quantity_Id  
-        AND specie.FK_Gender_Id = Gender.Gender_Id  
-        AND Gender.FK_Family_Id = Family.Family_Id 
+        AND specie.FK_Beak_Type_ID = Beak_Type_Id
+        AND specie.FK_Gender_Id = Gender.Gender_Id
+        AND Gender.FK_Family_Id = Family.Family_Id
         AND Family.FK_Sub_Order_Id = sub_order.Sub_Order_Id 
         AND sub_order.FK_Order_Id = Orders.Order_Id 
-        AND Orders.FK_Class_Id = Class.Class_Id; 
+        AND Orders.FK_Class_Id = Class.Class_Id
+        ORDER BY Specie.Specie_Name;
 END
 
 CREATE DEFINER=`DBadmin`@`localhost` PROCEDURE `find_species_detail`(
@@ -71,19 +73,51 @@ CREATE DEFINER=`DBadmin`@`localhost` PROCEDURE `find_bird`(
     p_Class_Name VARCHAR(45)
 )
 BEGIN
-	SELECT Species_Id, Specie_Name, Size_Name, Habitat_Name, Beak_Name, Color_Name, Quantity, Gender_Name, Family_Name, Sub_Order_Name, Order_Name, Class_Name, Description, 
-    Register_Date, Username, First_Name, Last_Name
+	SELECT 
+    Specie_Name, 
+    Species_Id, 
+    ScientificName, 
+    Size_Name, 
+    Habitat_Name, 
+    Beak_Name, 
+    Color_Name, 
+    Quantity, 
+    Gender_Name, 
+    Family_Name, 
+    Sub_Order_Name, 
+    Order_Name, 
+    Class_Name, 
+    Description, 
+    Register_Date, 
+    Username, 
+    First_Name, 
+    Last_Name
     FROM Species_found, Specie, Size, Habitat, Beak_Type, Color, Offspring_Quantity, Gender, Family, Sub_Order, Orders, Class, user_table
-    WHERE Specie.Specie_Name = COALESCE(p_Specie_Name, specie.Specie_Name)
-	||'%' and Size.Size_Name = COALESCE(p_Size_Name, Size.Size_Name)
-    ||'%' and Habitat.Habitat_Name = COALESCE(p_Habitat_Name, Habitat.Habitat_Name) 
-    ||'%' and Beak_Type.Beak_Name = COALESCE(p_Beak_Name, Beak_Type.Beak_Name)
-    ||'%' and Color.Color_Name = COALESCE(p_Color_Name, Color.Color_Name)
-    ||'%' and Offspring_Quantity.Quantity = COALESCE(p_Quantity, Offspring_Quantity.Quantity)
-    ||'%' and Gender.Gender_Name = COALESCE(p_Gender_Name, Gender.Gender_Name)
-    ||'%' and Family.Family_Name = COALESCE(p_Family_Name, Family.Family_Name)
-    ||'%' and Sub_Order.Sub_Order_Name = COALESCE(p_Sub_Order_Name, Sub_Order.Sub_Order_Name)
-    ||'%' and `Orders`.Order_Name = COALESCE(p_Order_Name, `Orders`.Order_Name)
-    ||'%' and Class.Class_Name = COALESCE(p_Class_Name, Class.Class_Name)
+    WHERE (
+    Specie.Specie_Name = COALESCE(p_Specie_Name, specie.Specie_Name) 
+	AND Size.Size_Name = COALESCE(p_Size_Name, Size.Size_Name)
+    AND Habitat.Habitat_Name = COALESCE(p_Habitat_Name, Habitat.Habitat_Name)
+    AND Beak_Type.Beak_Name = COALESCE(p_Beak_Name, Beak_Type.Beak_Name)
+    AND Color.Color_Name = COALESCE(p_Color_Name, Color.Color_Name)
+    AND Offspring_Quantity.Quantity = COALESCE(p_Quantity, Offspring_Quantity.Quantity)
+    AND Gender.Gender_Name = COALESCE(p_Gender_Name, Gender.Gender_Name) 
+    AND Family.Family_Name = COALESCE(p_Family_Name, Family.Family_Name) 
+    AND Sub_Order.Sub_Order_Name = COALESCE(p_Sub_Order_Name, Sub_Order.Sub_Order_Name) 
+    AND `Orders`.Order_Name = COALESCE(p_Order_Name, `Orders`.Order_Name) 
+    AND Class.Class_Name = COALESCE(p_Class_Name, Class.Class_Name)
+    ) AND (
+    Specie.Specie_Id = Species_Found.FK_Specie_ID 
+    AND User_Table.User_Id = Species_Found.FK_User_ID
+    AND Specie.FK_Size_ID = Size.Size_ID
+    AND specie.FK_Habitat_Id = habitat.Habitat_Id
+    AND Specie.FK_Beak_Type_ID = Beak_Type.Beak_Type_ID
+    AND specie.FK_Color_Id = Color.Color_Id
+    AND specie.FK_Offspring_Quantity_Id = offspring_quantity.Offspring_Quantity_Id
+    AND specie.FK_Gender_Id = Gender.Gender_Id
+    AND Gender.FK_Family_Id = Family.Family_Id 
+    AND Family.FK_Sub_Order_Id = sub_order.Sub_Order_Id 
+    AND Sub_Order.FK_Order_Id = Orders.Order_Id 
+    AND Orders.FK_Class_Id = Class.Class_Id
+    )
     ORDER BY Specie_Name;
 END
